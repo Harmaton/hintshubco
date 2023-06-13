b6fa887399f6f2b624b3adcf18a9650ed7b2287a
Paul Ochieng Okello <paulokello273@gmail.com>
2023-06-13 07:58:34 +0300

    👌 IMPROVE: loaders

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import { Heading, Spinner } from "@chakra-ui/react"
+import { Heading, Spinner, useColorModeValue } from "@chakra-ui/react"
 
 export default function loading() {
+  const buttonColor = useColorModeValue('white', 'gray.900')
   return (
-    <div className="flex items-center justify-center h-screen bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-gray-900 to-gray-600">
-    <div className="">  <Heading size={"lg"} className="animate-bounce">
-            Loading...
+    <div className="min-h-screen bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-gray-900 to-gray-600 flex justify-center items-center">
+      <div className="flex justify-center items-center flex-col">
+        <Heading size={"lg"} className="animate-bounce dark:text-white text-slate-900">
+          Loading...
         </Heading>
-        <Spinner size='xl' /></div>  
+        <Spinner size='xl' className="dark:text-white text-slate-900" />
+      </div>
     </div>
   )
 }
-
